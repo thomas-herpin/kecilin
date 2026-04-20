b3a25e2e23419d6527c11cd59d453093da6484de
Thomas Herpin <thomasherpin312@gmail.com>
2026-04-21 00:07:58 +0700

    include file js home dan history dalam vite

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,11 @@ import tailwindcss from '@tailwindcss/vite';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: ['resources/css/app.css', 
+                    'resources/js/app.js',
+                    'resources/js/home.js',
+                    'resources/js/history.js',
+                ],
             refresh: true,
         }),
         tailwindcss(),
